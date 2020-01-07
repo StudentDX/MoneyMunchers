c23fdc95c142341c98fbc0c2c18898d797dc8adc
vsofat <vsofat00@stuy.edu>
2020-01-07 08:25:28 -0500

    starting to create login page

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
 from app import app
+import requests
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# if logged in: welcome page, else login page
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if 'user' in session: #checks that a user is logged into a session, render welcome page
+        print("Session username: " + session['user'])
+        flash ("You are logged in.")
+        print(session['user'])
+        return render_template("base.html", username = str(session['user']))
+
+    return render_template("login.html") #if not, then render login page
