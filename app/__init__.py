f843368e9b1169251bf2e2995a0c46dd570ca47f
Xyresic <ericjmlam@gmail.com>
2020-01-07 08:31:39 -0500

    created database

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,13 @@ from app.models import db, Users, Expenses
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 @app.route('/home')
 def home():
