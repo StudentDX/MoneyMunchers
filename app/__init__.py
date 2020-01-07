41a4efffe7d2f5d01b602a429409946784873556
Emory Walsh <ewalsh00@stuy.edu>
2020-01-07 08:38:01 -0500

    Starting to make templates

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
 @app.route('/')
+def hello():
+    return render_template('home.html')
 @app.route('/home')
 def home():
     return render_template('home.html')
