16c81e5cdcd51a5eb1024f71df3e324d6199c1b0
vsofat <vsofat00@stuy.edu>
2020-01-07 08:29:24 -0500

    login page

--- run.py
+++ run.py
@@ -14,3 +14,16 @@ def home():
         return render_template("base.html", username = str(session['user']))
 
     return render_template("login.html") #if not, then render login page
+
+# login page
+@app.route("/auth", methods=['POST'])
+def login():
+    username = request.form.get('user')
+    password = request.form.get('pw')
+
+    #if (db_ops.authenticate(username, password)):
+    #    session['user'] = username
+    #    return redirect(url_for('home')) #should trigger if statement in "/" route
+
+    flash("Failed to log in. The username or password provided did not match any accounts.")
+    return redirect(url_for('home'))
