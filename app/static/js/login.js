e6dc41df52731ba93fd7a4bbdbed9bfa1f464010
Xyresic <ericjmlam@gmail.com>
2020-01-08 23:16:22 -0500

    improved sign up and log in pages

---
+++ app/static/js/login.js
@@ -0,0 +1,12 @@
+var user_field = document.getElementById('username');
+user_field.setAttribute('id','user-field');
+user_field.setAttribute('class','form-control');
+user_field.setAttribute('placeholder','e.g. moneymuncher');
+
+var pass_field = document.getElementById('password');
+pass_field.setAttribute('id','pass-field');
+pass_field.setAttribute('class','form-control');
+pass_field.setAttribute('placeholder','Minimum 8 Characters');
+
+var button = document.getElementById('submit');
+button.setAttribute('class','btn btn-primary');
